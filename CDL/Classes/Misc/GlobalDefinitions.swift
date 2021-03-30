c77ca6aaa4dfc3ff629255fa0c680124ca60e81f
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2021-03-30 11:36:46 +0300

    minor

--- CDL/Classes/Misc/GlobalDefinitions.swift
+++ CDL/Classes/Misc/GlobalDefinitions.swift
@@ -12,7 +12,7 @@ final class GlobalDefinitions {
     static let sdkDomainUrl = "https://analytics.korrekted.com"
     static let sdkApiKey = "ojDG5FBsVOOcRyXP"
     
-    static let applicationTag = "CDL"
+    static let applicationTag = "EXAM_CDL"
     
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
