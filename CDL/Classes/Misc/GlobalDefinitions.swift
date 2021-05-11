d6dd10aa2ebafca762be6dc5f8541a272452bd7f
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2021-05-11 11:33:10 +0300

    обновил креды; билд 2

--- CDL/Classes/Misc/GlobalDefinitions.swift
+++ CDL/Classes/Misc/GlobalDefinitions.swift
@@ -6,18 +6,18 @@
 //
 
 final class GlobalDefinitions {
-    static let domainUrl = "https://exams.mapo.la"//"https://exams.korrekted.com"
-    static let apiKey = "GS3Fx34F3dxuy2LM"//"gklmVPNauDHNO0Dn"
+    static let domainUrl = "https://exams.mapo.la"
+    static let apiKey = "GS3Fx34F3dxuy2LM"
     
-    static let sdkDomainUrl = "https://analytics.korrekted.com"
-    static let sdkApiKey = "uJRANDiMDhqUfp7b"//"ojDG5FBsVOOcRyXP"
+    static let sdkDomainUrl = "https://analytics.korrekted.com "
+    static let sdkApiKey = "uJRANDiMDhqUfp7b"
     
-    static let applicationTag = "EXAM_CDL"
+    static let applicationTag = "PREP_CDL"
     
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
     
-    static let appStoreId = "1553891079"
+    static let appStoreId = "1566343337"
     
     static let termsOfServiceUrl = "https://docs.google.com/document/d/1-W9vqg-sno9DcYcDo-XhT26TMhYE1mOU6OivK2IhI7M/edit"
     static let privacyPolicyUrl = "https://docs.google.com/document/d/1GCIelmdgxFi7_dLgg0O_RzfHZB3PMYlczk9gekCRlIE/edit"
