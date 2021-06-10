ebe38e73a97502698a373f9dd6ceff27078d6cdf
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2021-06-10 22:09:17 +0300

    make flashcards

--- CDL/Classes/Presentation/Flashcards/FlashcardsViewModel.swift
+++ CDL/Classes/Presentation/Flashcards/FlashcardsViewModel.swift
@@ -10,4 +10,25 @@ import RxCocoa
 
 final class FlashcardsViewModel {
     lazy var flashcardTopicId = BehaviorRelay<Int?>(value: nil)
+    
+    lazy var flashcards = makeFlashcards()
+    
+    private lazy var flashcardsManager = FlashcardsManagerCore()
+}
+
+// MARK: Private
+private extension FlashcardsViewModel {
+    func makeFlashcards() -> Driver<[Flashcard]> {
+        flashcardTopicId.compactMap { $0 }
+            .flatMapLatest { [weak self] flashcardTopicId -> Single<[Flashcard]> in
+                guard let self = self else {
+                    return .never()
+                }
+                
+                return self.flashcardsManager
+                    .obtainFlashcards(flashcardTopicId: flashcardTopicId)
+                    .catchAndReturn([])
+            }
+            .asDriver(onErrorJustReturn: [])
+    }
 }
