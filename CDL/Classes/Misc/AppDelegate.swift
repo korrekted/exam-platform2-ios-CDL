42b149c48867943d8a17ff398e077b5ee388f7a2
akonst17 <akonst17@gmail.com>
2022-07-28 17:26:20 +0300

    minor; build 44

--- CDL/Classes/Misc/AppDelegate.swift
+++ CDL/Classes/Misc/AppDelegate.swift
@@ -22,6 +22,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         
+        NumberLaunches().launch()
+        
         let vc = SplashViewController.make(generateStep: generateStepInSplash.asSignal())
         window?.rootViewController = vc
         window?.makeKeyAndVisible()
